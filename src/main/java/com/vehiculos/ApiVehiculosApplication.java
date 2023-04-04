e66ce89e76b2838afed18faba53c4969b89ff44f
Cristo Carrillo <cjcarrillos@ufpso.edu.co>
2023-04-03 20:52:56 -0500

    ➕ ADD: Punto inical del proyecto

---
+++ src/main/java/com/vehiculos/ApiVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.vehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVehiculosApplication.class, args);
+	}
+
+}
